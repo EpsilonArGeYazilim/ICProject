6501528bb4d8a1399a4b88e32808314003603b2d
korayaslaan <koray.aslan.44@gmail.com>
2021-03-30 10:38:33 +0300

    KORAY

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -29,14 +29,15 @@
           <div id="tab-1" class="container tab-pane active">
             <img width="100%" />
             <input
-              v-for="(item, index) in resultCategory.images"
+              v-for="(item, index) in categoryDatas"
               :key="index"
               type="image"
-              :src="img_base_url + 'Img/' + item.img_url"
-              @click="getByIdSubCategory(index)"
+              src="https://icdn.ensonhaber.com/resimler/galeri/206478.jpg"
+              @click="getByIdCategoryAllDatas(categoryDatas[index].category_id)"
               width="100%"
               height="80%"
             />
+          
           </div>
 
           <!-- üst kısım ambiance kısmı -->
@@ -46,28 +47,18 @@
             <!-- subcategory isimlerinin çekildiği yer BURASI DEĞİŞMEYECEK SUBCATEGORY NAME SABİT-->
             <ul class="nav nav-tabs tabLi" style="width: 250px">
               <li class="nav-item">
-                <a
-                  class="nav-link"
-                  data-toggle="tab"
-                  href="#tab-3"
-                  @click="onChange(0)"
-                  >{{ subName1 }}</a
-                >
+                <a class="nav-link" data-toggle="tab" href="#tab-3">{{
+                  subName1
+                }}</a>
               </li>
               <li class="nav-item">
-                <a
-                  class="nav-link"
-                  data-toggle="tab"
-                  href="#tab-4"
-                  @click="onChange(1)"
-                  >{{ subName2 }}</a
-                >
+                <a class="nav-link" data-toggle="tab" href="#tab-4">{{
+                  subName2
+                }}</a>
               </li>
             </ul>
-            <!-- subcategory isimlerinin çekildiği yer -->
-
             <div
-              v-for="(item, index) in imgColor[subNumber]"
+              v-for="(item, index) in color1Datas"
               :key="index"
               class="tab-pane fade show active"
               id="countertop"
@@ -77,14 +68,36 @@
               <input
                 class="icons"
                 type="image"
-                :src="img_base_url + 'ImgColor/' + item.img_color_url"
+                :src="img_base_url + 'ImgColor/' + item.color1_url"
                 alt=""
-                @click="getName('1', '5')"
+                @click="getName(item.img_url)"
                 width="80px"
                 height="80px"
                 style="border-radius: 50%"
               />
               <!--UFAK RESİMLER ÜSTTEN ÇEKİLİYO-->
+            </div>
+
+            <div
+              v-for="(item, index) in color2Datas"
+              :key="index"
+              class="tab-pane fade show active"
+              id="countertop"
+              role="tabpanel"
+              style="float: left; padding: 5px"
+            >
+        
+              <input
+                class="icons"
+                type="image"
+                :src="img_base_url + 'ImgColor/' + item.color2_url"
+                alt=""
+                @click="getName(item.img_url)"
+                width="80px"
+                height="80px"
+                style="border-radius: 50%"
+              />
+              <!--UFAK RESİMLER ÜSTTEN ÇEKİLİYO 2-->
             </div>
           </div>
         </div>
@@ -106,15 +119,18 @@ import store from "../store";
 export default {
   data() {
     return {
+      imagesDatas: {},
       color1_id: [],
       color2_id: [],
-      color1:{ },
-      color2:{ },
-      categories: {},
-      images: {},
+      color1Datas: {},
+      color2Datas: {},
+      categoryDatas: {},
+      color1: "",
+      color2: "",
+      color1ids: [],
+      color2ids: [],
       image: "",
-      Color1:"",
-      Color2:"",
+      category_id: "",
       subNumber: 0,
       subName1: "",
       subName2: "",
@@ -136,9 +152,8 @@ export default {
     return axios
       .get(dataUrl)
       .then((response) => {
-        // console.log(response);
 
-        this.resultCategory = response.data;
+        this.categoryDatas = response.data.categories;
       })
       .catch((err) => {
         console.log(err.response);
@@ -160,58 +175,89 @@ export default {
       //console.log(this.img);
     },
     color1data() {
-      for(item in images){
-        if(item.color2==[]){
-            item.Color1.id}
-            color1_id.add(item.color1_id)
+      for (item in images) {
+        if (item.color2 == []) {
+          item.Color1.id;
+        }
+        color1_id.add(item.color1_id);
       }
       //console.log(this.img);
     },
     color2data() {
-      for(item in images){
-        if(item.color1==[]){
-            item.Color2.id}
-            color2_id.add(item.color2_id)
+      for (item in images) {
+        if (item.color1 == []) {
+          item.Color2.id;
+        }
+        color2_id.add(item.color2_id);
       }
       //console.log(this.img);
     },
-    getName(name, name2) {
-      this.name = name;
-      //console.log(this.
-      this.ImgName = name + "_" + name2 + ".jpg";
-      this.img = this.ImgName;
-    },
     imageChange() {
-      if(item.Color1 == [] && images.Color2 == []){
-        image = item.img_url
-      }
-    image = Color1 + "_" +Color2+ "_" +this.category_id+ ".jpg"
- 
+      this.image =
+        this.color1 + "_" + this.color2 + "_" + this.category_id + ".jpg";
     },
     getImage2(img_url) {
       this.img = img_url;
       //console.log(this.img);
     },
-    onChange(number) {
-      this.subNumber = number;
+    color1Click(color1_id) {
+      this.color1 = color1_id;
+      color2Changeids();
+      imageChange();
     },
-    getByIdSubCategory(category_id) {
-      var subCategory = this.subCategory;
-      //console.log(category_id);
+    color2Click(color2_id) {
+      this.color1 = color2_id;
+      color1Changeids();
+      imageChange();
+    },
+    color2Changeids() {
+      this.color1 = color2_id;
+      this.color2ids = [];
+      for (item in this.imagesDatas) {
+        if (item.color1_id == this.color1) {
+          this.color2ids.add.item(item.color2_id);
+        }
+      }
+    },
+    color1Changeids() {
+      this.color2 = color1_id;
+      this.color1ids = [];
+      for (item in this.imagesDatas) {
+        if (item.color2_id == this.color2) {
+          this.color1ids.add.item(item.color1_id);
+        }
+      }
+    },
+
+    getByIdCategoryAllDatas(category_id) {
+
+      this.category_id = category_id;
+      console.log(category_id);
+      for (var index in this.categoryDatas) {
+
+        if (this.categoryDatas[index].category_id == this.category_id) {
+          this.subName1 = this.categoryDatas[index].sub1_name;
+          this.subName2 = this.categoryDatas[index].sub2_name;
+        }
+      }
+
+     
       var url =
-        store.state.base_url + "Category/getByIdSubCategory.php?key=123";
+        store.state.base_url + "Category/getByIdCategoryAllDatas.php?key=123";
       var datas = {
         category_id: category_id,
       };
       return axios
-
         .post(url, JSON.stringify(datas))
         .then((response) => {
-          this.subName1 = response.data.sub_categories[0].name;
-          this.subName2 = response.data.sub_categories[1].name;
-          this.imgColor = response.data.sub_images;
-          this.getImage2(this.imgColor[0][0].img_url);
-          //console.log(subCategory);
+
+
+          this.color1Datas = response.data.color1datas;
+          this.color2Datas = response.data.color2datas;
+            console.log(this.color1Datas);
+      
+        
+        
         })
         .catch((error) => {
           console.log(error.response);
