1cbba131af09c6039e165bfcaf4aeafb4d6b309a
korayaslaan <koray.aslan.44@gmail.com>
2021-03-31 16:51:44 +0300

    korayfinish

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,124 +1,154 @@
 <template>
-<div :style="'background-image: url('+img_base_url + 'Img/' + image+');'">
-{{image}}
-  <div class="row" style="height: 655px">
-    <nav id="tmSidebar" class="tm-bg-black-transparent tm-sidebar">
-      <button
-        class="navbar-toggler"
-        type="button"
-        aria-label="Toggle navigation"
-      >
-        <i class="fas fa-bars"></i>
-      </button>
-
-      <div class="container mt-2">
-        <h2>Epsilon Arge Yazılım</h2>
-
-        <!-- Nav tabs -->
-        <ul class="nav nav-tabs tabLi">
-          <li class="nav-item">
-            <a class="nav-link active" data-toggle="tab" href="#tab-1"
-              >Ambiance</a
-            >
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="#tab-2">Colors</a>
-          </li>
-        </ul>
-
-        <!-- Tab panes BURDA CAT_İD YE GÖRE STANDART BELİRLENECEK-->
-        <div class="tab-content tabContainer">
-          <div id="tab-1" class="container tab-pane active">
-            <img width="100%" />
-            <input
-              v-for="(item, index) in categoryDatas"
-              :key="index"
-              type="image"
-              :src="img_base_url + 'Img/' + categoryImages[index].img_url"
-              @click="getByIdCategoryAllDatas(categoryDatas[index].category_id)"
-              width="100%"
-              height="80%"
-            />
-          </div>
-
-          <!-- üst kısım ambiance kısmı -->
-          <div id="tab-2" class="container tab-pane">
-            <br />
-            <img width="100%" />
-            <!-- subcategory isimlerinin çekildiği yer BURASI DEĞİŞMEYECEK SUBCATEGORY NAME SABİT-->
-            <ul class="nav nav-tabs tabLi" style="width: 250px">
-              <li class="nav-item">
-                <a
-                  class="nav-link active"
-                  data-toggle="tab"
-                  href="#tab-3"
-                  @click="onChange(0)"
-                  >{{ subName1 }}</a
-                >
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  data-toggle="tab"
-                  href="#tab-4"
-                  @click="onChange(1)"
-                  >{{ subName2 }}</a
-                >
-              </li>
-            </ul>
-            <div
-              v-if="tabNumber == 0"
-              class="tab-pane fade show active"
-              id="tab-3"
-              role="tabpanel"
-              style="float: left; padding: 5px"
-            >
-              <input
-                v-for="(item, index) in color1ids"
-                :color1_id="item"
-                :key="index"
-                class="icons"
+  <div>
+    <div
+      class="row"
+      :style="
+        'background-image: url(' +
+        img_base_url +
+        'Img/' +
+        image +
+        ');transition: background 1s linear;overflow: hidden;-ms-transition: background 1s linear;-o-transition: background 1s linear; opacity: 1.0;-moz-transition: background 1s linear; -webkit-transition: background 1s linear; height: 100vh; min-height: 350px;  background-position: center; background-repeat: no-repeat;background-size: cover;'
+      "
+    >
+      <nav id="tmSidebar" class="tm-bg-black-transparent tm-sidebar">
+        <button
+          class="navbar-toggler"
+          type="button"
+          aria-label="Toggle navigation"
+        >
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="container mt-2">
+          <h2 style="color: white">Epsilon Arge Yazılım</h2>
+          <ul class="nav nav-tabs tabLi">
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                data-toggle="tab"
+                href="#tab-1"
+                style="font-size: 15px"
+                >Ambiance</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link"
+                data-toggle="tab"
+                href="#tab-2"
+                style="font-size: 15px"
+                >Colors</a
+              >
+            </li>
+          </ul>
+          <br>
+          <div class="tab-content tabContainer">
+            <div id="tab-1" class="container tab-pane active" >
+              <div style="cursor: pointer" v-for="(item, index) in categoryDatas"
+                :key="index">
+              <img
+                
                 type="image"
-                :src="img_base_url + 'ImgColor/' + color1Datas[item].color1_url"
-                alt=""
-                @click="color1Click(color1Datas[item].color1_id)"
-                width="80px"
-                height="80px"
-                style="border-radius: 50%"
+                :src="img_base_url + 'Img/' + categoryImages[index].img_url"
+                @click="
+                  getByIdCategoryAllDatas(categoryDatas[index].category_id)
+                "
+                width="100%"
+                height="80%"
               />
-       
-              <!--UFAK RESİMLER ÜSTTEN ÇEKİLİYO-->
+               <p style="color: #f0f7ff; text-align: center ; text-transform: uppercase;font-size: 12px">
+                    {{ categoryDatas[index].category_name }}
+                  </p></div>
             </div>
-
-            <div
-              v-if="tabNumber == 1"
-              class="tab-pane fade"
-              id="tab-4"
-              role="tabpanel"
-              style="float: left; padding: 5px"
-            >
-              <input
-                v-for="(item, index) in color2ids"
-                :color2_id="item"
-                :key="index"
-                class="icons"
-                type="image"
-                :src="img_base_url + 'ImgColor/' + color2Datas[item].color2_url"
-                alt=""
-                @click="color2Click(color2Datas[item].color2_id)"
-                width="80px"
-                height="80px"
-                style="border-radius: 50%"
-              />
-              <!--UFAK RESİMLER ÜSTTEN ÇEKİLİYO 2-->
+            <div id="tab-2" class="container tab-pane">
+              <br />
+              <ul class="nav nav-tabs tabLi" style="width: 250px">
+                <li class="nav-item">
+                  <a
+                    class="nav-link active"
+                    data-toggle="tab"
+                    style="font-size: 15px"
+                    href="#tab-3"
+                    @click="onChange(0)"
+                    >{{ subName1 }}</a
+                  >
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link"
+                    data-toggle="tab"
+                    style="font-size: 15px"
+                    href="#tab-4"
+                    @click="onChange(1)"
+                    >{{ subName2 }}</a
+                  >
+                </li>
+              </ul>
+              <div
+                v-if="tabNumber == 0"
+                class="tab-pane fade row show active"
+                id="tab-3"
+                role="tabpanel"
+                style="float: left; padding: 5px"
+              >
+                <div
+                  v-for="(item, index) in color1ids"
+                  :color1_id="item"
+                  :key="index"
+                  class="col-md-6"
+                >
+                  <img
+                    class="icons"
+                    type="image"
+                    :src="
+                      img_base_url + 'ImgColor/' + color1Datas[item].color1_url
+                    "
+                    alt=""
+                    @click="color1Click(color1Datas[item].color1_id)"
+                    width="80px"
+                    height="80px"
+                    style="border-radius: 50%; margin: 4px; cursor: pointer"
+                  />
+                  <p style="color: #f0f7ff; text-align: center ; text-transform: uppercase;font-size: 12px">
+                    {{ color1Datas[item].color1_name }}
+                  </p>
+                </div>
+              </div>
+              <div
+                v-if="tabNumber == 1"
+                class="tab-pane fade row"
+                id="tab-4"
+                role="tabpanel"
+                style="float: left; padding: 5px"
+              >
+                <div
+                  class="col-md-6"
+                  v-for="(item, index) in color2ids"
+                  :color2_id="item"
+                  :key="index"
+                >
+                  <img
+                    class="icons"
+                    type="image"
+                    :src="
+                      img_base_url + 'ImgColor/' + color2Datas[item].color2_url
+                    "
+                    alt=""
+                    @click="color2Click(color2Datas[item].color2_id)"
+                    width="80px"
+                    height="80px"
+                    style="border-radius: 50%; margin: 4px; cursor: pointer"
+                  />
+                  <p style="color: #f0f7ff; text-align: center;text-transform: uppercase;font-size: 14px">
+                    {{ color2Datas[item].color2_name }}
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
-
+      </nav>
+    </div>
   </div>
-</div>
 </template>
 <script>
 import axios from "axios";
@@ -154,8 +184,6 @@ export default {
   },
 
   mounted: function () {
-
-
     let dataUrl = store.state.base_url + "Category/getAllCategory.php?key=123";
     return axios
       .get(dataUrl)
@@ -163,10 +191,9 @@ export default {
         this.categoryDatas = response.data.categories;
         this.categoryImages = response.data.categoryFirstImage;
         for (var index in this.categoryImages) {
-          this.image = this.categoryImages[index].img_url; // geçici
+          this.image = this.categoryImages[index].img_url;
           break;
         }
-
       })
       .catch((err) => {
         console.log(err.response);
@@ -174,18 +201,15 @@ export default {
   },
   methods: {
     imageChange() {
-
-
-      for(var index in this.imagesDatas)
-      {
-      
-        if((this.imagesDatas[index].color1_id == this.color1) && (this.imagesDatas[index].color2_id == this.color2))
-        {
+      for (var index in this.imagesDatas) {
+        if (
+          this.imagesDatas[index].color1_id == this.color1 &&
+          this.imagesDatas[index].color2_id == this.color2
+        ) {
           this.image = this.imagesDatas[index].img_url;
           break;
         }
       }
-
     },
     onChange(number) {
       this.tabNumber = number;
@@ -195,33 +219,26 @@ export default {
       this.color1 = color1_id;
       this.imageChange();
       this.color2Changeids();
-    
     },
     color2Click(color2_id) {
       this.color2 = color2_id;
       this.imageChange();
       this.color1Changeids();
-      
     },
     color2Changeids() {
       this.color2ids = [];
       for (var item in this.imagesDatas) {
-    
         if (this.imagesDatas[item].color1_id == this.color1) {
-            console.log(this.imagesDatas[item])
+          console.log(this.imagesDatas[item]);
           this.color2ids.push(this.imagesDatas[item].color2_id);
-         
         }
       }
-
     },
     color1Changeids() {
       this.color1ids = [];
       for (var item in this.imagesDatas) {
-
         if (this.imagesDatas[item].color2_id == this.color2) {
-            this.color1ids.push(this.imagesDatas[item].color1_id);
-          
+          this.color1ids.push(this.imagesDatas[item].color1_id);
         }
       }
     },
@@ -250,27 +267,21 @@ export default {
           this.color2Datas = response.data.color2datas;
           this.imagesDatas = response.data.imagesdatas;
 
+          this.color1 = this.imagesDatas[0].color1_id;
+          this.color2 = this.imagesDatas[0].color2_id;
 
-          this.color1=this.imagesDatas[0].color1_id;
-          this.color2=this.imagesDatas[0].color2_id;
+          imageChange();
 
-        imageChange();
-
-        for (var item in this.imagesDatas) {
-
-          this.color1ids.push(this.imagesDatas[item].color1_id);
-           this.color2ids.push(this.imagesDatas[item].color2_id);
-
-            }
-        
+          for (var item in this.color1Datas) {
+            this.color1ids.push(this.color1Datas[item].color1_id);
+          }
+          for (var item in this.color2Datas) {
+            this.color2ids.push(this.color2Datas[item].color2_id);
+          }
         })
         .catch((error) => {
           console.log(error.response);
         });
-
-
-    
-
     },
   },
   components: {},
